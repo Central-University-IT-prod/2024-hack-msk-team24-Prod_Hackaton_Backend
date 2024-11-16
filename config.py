016c9eeabca3367fc6ca9e03e464467ca8208c66
prod-tech <support@centraluniversity.ru>
2024-11-17 02:32:52 +0300

    init commit

---
+++ config.py
@@ -0,0 +1,5 @@
+PORT = 4000
+ENGINE = f'postgresql+asyncpg://{{sensitive_data}}:{{sensitive_data}}@prod-postgres:5432/prod'
+ADMIN_USERNAME = '{{sensitive_data}}'
+ADMIN_PASSWORD = '{{sensitive_data}}'
+FNS_TOKEN = "{{sensitive_data}}"
